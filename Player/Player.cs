d80fed65bd23ffae2a6e82e180a40ca2569ddce1
LiTe1991 <vadim19917@gmail.com>
2025-10-27 20:08:15 +0100

    feat: implement block with collision and movement throug char

--- Player/Player.cs
+++ Player/Player.cs
@@ -4,10 +4,11 @@ namespace Player;
 
 public partial class Player : CharacterBody2D
 {
-	private const float MoveSpeed = 100;
+	[Export]
+	private float MoveSpeed = 70;
+	[Export]
+	private float PushStreng = 5;
 
-	private Vector2 moveVector;
-	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -17,33 +18,50 @@ public partial class Player : CharacterBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		moveVector = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+		Vector2 moveVector = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		Velocity = moveVector * MoveSpeed;
 
 		PlayAnimationForDirection();
+		CheckCollision();
 
 		MoveAndSlide();
 	}
-	
+
 	private void PlayAnimationForDirection()
 	{
 		AnimatedSprite2D playNode = GetNode<AnimatedSprite2D>("MoveAnimation");
 
-        if(Velocity.X > 0)
-        {
+		if (Velocity.X > 0)
+		{
 			playNode.Play("move_right");
-        } else if(Velocity.X < 0)
-        {
-            playNode.Play("move_left");
-        } else if(Velocity.Y > 0)
-        {
-            playNode.Play("move_down");
-        } else if(Velocity.Y < 0)
-        {
-            playNode.Play("move_up");
-        } else
-        {
+		}
+		else if (Velocity.X < 0)
+		{
+			playNode.Play("move_left");
+		}
+		else if (Velocity.Y > 0)
+		{
+			playNode.Play("move_down");
+		}
+		else if (Velocity.Y < 0)
+		{
+			playNode.Play("move_up");
+		}
+		else
+		{
 			playNode.Stop();
-        }
+		}
+	}
+	
+	private void CheckCollision()
+    {
+		KinematicCollision2D collision2D = GetLastSlideCollision();
+		if (collision2D == null) return;
+
+		RigidBody2D collider = collision2D.GetCollider() as RigidBody2D;
+		if (collider == null) return;
+
+		Vector2 collisionVector = collision2D.GetNormal();
+		collider.ApplyCentralForce(collisionVector * PushStreng * -1);
     }
 }
